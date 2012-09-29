e82423f50c8badd2b266d18e8d4a4ccaea53ed2a
Balint Pfliegel <balint.pfliegel@gmail.com>
2012-09-29 06:52:50 -0700

    Windows RT support #4 - Test project added

---
+++ src/WindowsRTTester/Integration/WindowsRTPlug.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Portfish;
+using System.Text;
+using Windows.UI.Core;
+using System.Threading;
+using Windows.UI.Xaml;
+
+namespace WindowsRTTester
+{
+    internal sealed class WindowsRTPlug : IPlug
+    {
+        #region Communication
+
+        private void DispatchMessageFromEngine(string message)
+        {
+            // Dispatches the message to the UI/Application logic
+            // =====================================================================
+            // TODO: do your custom implementation here
+            // =====================================================================
+            MainPage.RaiseEngineMessage(message);
+        }
+
+        public void SendMessageToEngine(string message)
+        {
+            // Sends a message to the engine
+            SendMessageToEngineInternal(message);
+        }
+
+        #endregion
+
+        #region Internal methods
+
+        #region Write
+
+        // Hold a buffer until we get a full line
+        private StringBuilder _sb = new StringBuilder();
+        private void WriteInternal(string message)
+        {
+            _sb.Append(message);
+            if (message.IndexOf("\n")>-1)
+            {
+                string wholeMessage = _sb.ToString();
+                _sb.Clear();
+
+                // Do something with the message
+                DispatchMessageFromEngine(wholeMessage);
+            }
+        }
+
+        #endregion
+
+        #region Read
+
+        AutoResetEvent _readEvent = new AutoResetEvent(false);
+        private string _message = string.Empty;
+
+        private void SendMessageToEngineInternal(string message)
+        {
+            _message = message;
+            _readEvent.Set();
+        }
+
+        private string ReadInternal()
+        {
+            _readEvent.WaitOne();
+            return _message;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Interface methods
+
+        public void Write(string message)
+        {
+            WriteInternal(message);
+        }
+
+        public string ReadLine()
+        {
+            return ReadInternal();
+        }
+
+        #endregion
+    }
+}
---
+++ src/WindowsRTTester/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowsRTTester
+{
+    public delegate void EngineMessageHandler(EngineMessageEventArgs e);
+
+    public class EngineMessageEventArgs : EventArgs
+    {
+        public readonly string Message;
+
+        public EngineMessageEventArgs(string message)
+        {
+            this.Message = message;
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            WindowsRTEngine.StartEngine();
+            EngineMessage += new EngineMessageHandler(AppendMessage);
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            dt.Tick += MessageUpdate;
+            dt.Start();
+        }
+
+        private string _lastMessage = string.Empty;
+
+        void MessageUpdate(object sender, object e)
+        {
+            string thisMessage = _sbEngineMessages.ToString();
+            if (thisMessage != _lastMessage)
+            {
+                txtEngine.Text = thisMessage;
+                _lastMessage = thisMessage;
+                scrScroller.ScrollToVerticalOffset(txtEngine.RenderSize.Height);
+            }
+        }
+
+        public static event EngineMessageHandler EngineMessage;
+
+        public static void RaiseEngineMessage(string message)
+        {
+            EngineMessageEventArgs e = new EngineMessageEventArgs(message);
+            if (EngineMessage != null) { EngineMessage(e); }
+        }
+
+        private StringBuilder _sbEngineMessages = new StringBuilder();
+
+        private void AppendMessage(EngineMessageEventArgs e)
+        {
+            _sbEngineMessages.Append(e.Message);
+        }
+
+        private void btnSendClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtMessageKeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        private void SendMessage()
+        {
+            WindowsRTEngine.ThePlug.SendMessageToEngine(txtMessage.Text);
+            txtMessage.Text = string.Empty;
+        }
+    }
+}
